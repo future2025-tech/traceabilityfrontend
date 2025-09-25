bf317d8a3b2576903ca8a8ec90158b7ba26e8003
Jaspher28 <jasphersujin2020@gmail.com>
2025-09-25 09:42:55 +0530

    product form header update

--- src/pages/AddProductForm.jsx
+++ src/pages/AddProductForm.jsx
@@ -1,18 +1,34 @@
-import { Stack, Typography } from '@mui/material'
+import { AppBar, Box, Breadcrumbs, IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 const AddProductForm = () => {
   return (
     <>
-        <Typography>Hello</Typography>
+      <Box
+        display="flex"
+        alignItems="center"
+        padding={2}
+        borderBottom="1px solid #eee"
+        bgcolor="#fafafa"
+      >
+        <IconButton size="small" sx={{ mr: 2 }}>
+          {/* <ArrowBackIosNewIcon fontSize="small" /> */}
+        </IconButton>
 
-        <Stack direction="row" spacing={2} border={1}>
+        <Box>
+          <Breadcrumbs aria-label="breadcrumb" sx={{ fontSize: 14, color: '#6c63ff' }}>
+            <Link underline="hover" color="inherit" href="/products">
+              My Products
+            </Link>
+          </Breadcrumbs>
 
-            {/* <Item >Item 1</Item>
-            <Item>Item 2</Item>
-            <Item>Item 3</Item> */}
-            <h1>hhh</h1>
-        </Stack>
+          <Typography variant="h6" fontWeight="bold">
+            Add Products
+          </Typography>
+        </Box>
+      </Box>
     </>
   )
 }
